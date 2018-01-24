5447e41cb0e0bb39f6dc5c3082797c49eed49337
Sword <ssword@SDGL1504e2435.id.tsheets.com>
2018-01-24 13:44:46 -0700

    Adds first pass at using parent config and updates the package.json file

--- bin/superyay.js
+++ bin/superyay.js
@@ -1,10 +1,23 @@
-const CURRENT_NODE_VERSION = '6.10.2';
-const CURRENT_NPM_VERSION = '5.5.1';
+const config = require('../../package.json');
+
+// Validation
+if (!config) {
+  return new Error('Unable to locate package.json');
+}
+
+if (!config.engines) {
+  return new Error('No versions of Node.js or npm set in package.json. Please add them under "engines".');
+} else if (!config.engines.node) {
+  return new Error('No node version set in package.json. Please add it under "engines".');
+} else if (!config.engines.npm) {
+  return new Error('No npm version set in package.json. Please add it under "engines".');
+}
+
 const getNodeVersion = require('../getNodeVersion.js');
 const getNpmVersion = require('../getNpmVersion.js');
 const checkVersions = require('../checkVersions.js');
 
 // Perform Check
-getNodeVersion(CURRENT_NODE_VERSION, CURRENT_NPM_VERSION)
+getNodeVersion(config.node, config.npm)
   .then(getNpmVersion)
   .then(checkVersions);
