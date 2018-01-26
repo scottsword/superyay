f3929aa24e5a44b45f2f44cc3c364d9e18a1212c
Sword <ssword@SDGL1504e2435.local>
2018-01-26 10:09:40 -0700

    Fixes error handling

--- bin/superyay.js
+++ bin/superyay.js
@@ -6,15 +6,23 @@ if (!config) {
   throw new Error('Unable to locate package.json');
 }
 
-if (!config.engines) {
-  const err = new Error('Superyay couldn\'t find any versions of Node.js or npm set in package.json. Please add them under "engines".');
-  throw err.message;
-} else if (!config.engines.node) {
-  const err = new Error('Superyay couldn\'t find a node version set in package.json. Please add it under "engines".');
-  throw err.message;
-} else if (!config.engines.npm) {
-  const err = new Error('Superyay couldn\'t find an npm version set in package.json. Please add it under "engines".');
-  throw err.message;
+try {
+    if (!config.engines) {
+        const err = new Error('Superyay couldn\'t find any versions of Node.js or npm set in package.json. Please add them under "engines".');
+        throw err.message;
+    } else if (!config.engines.node) {
+        const err = new Error('Superyay couldn\'t find a node version set in package.json. Please add it under "engines".');
+        throw err.message;
+    } else if (!config.engines.npm) {
+        const err = new Error('Superyay couldn\'t find an npm version set in package.json. Please add it under "engines".');
+        throw err.message;
+    }
+}
+
+catch(e) {
+  console.log('**************************************************************************************************************');
+  console.log('\033[31m ' + e + '\033[0m');
+  console.log('**************************************************************************************************************');
 }
 
 const getNodeVersion = require('../src/getNodeVersion.js');
@@ -24,4 +32,7 @@ const checkVersions = require('../src/checkVersions.js');
 // Perform Check
 getNodeVersion(config.engines, config.superyay)
   .then(getNpmVersion)
-  .then(checkVersions);
+  .then(checkVersions)
+  .catch((err) => {
+    console.log('Superyay Error: ' + err);
+  });
